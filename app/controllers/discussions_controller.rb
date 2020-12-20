d59cda1e014a763ff2f664db8cef51afbaeec012
ophelia swen <ophelia.swen@gmail.com>
2020-12-19 16:45:39 -0800

    trying to add image option to html but its not working yet..trying to figure this out

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -76,6 +76,6 @@ class DiscussionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def discussion_params
-      params.require(:discussion).permit(:title, :content, :channel_id)
+      params.require(:discussion).permit(:title, :content, :channel_id, :user_id, images: [])
     end
 end
